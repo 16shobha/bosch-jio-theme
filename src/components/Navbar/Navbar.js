0d60777967998adc86ce8371fc70a77c80c97d7c
Shah Ashutosh <Ashutosh.Shah@in.bosch.com>
2023-06-12 11:11:59 +0530

    fixed navbar for responsiveness and other link fixes in navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,39 +4,68 @@ import logo from '../../assets/jio_bp.png'
 import { Link } from "react-router-dom";
 const Navbar = () => {
     return (
-        <div>
-            <nav class="navbar navbar-expand-lg">
-                <a class="navbar-brand" href="#">
-                    <img className='logostyle' src={logo} alt="" />
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav">
-                        <li class="nav-item active">
-                        <Link to ="/home"><a class="nav-link" href="#">
-                                <i class="fa-solid fa-house"></i>Fleet</a></Link>
+        <>
+        <nav class="navbar navbar-expand-lg">
+  <div class="container-fluid">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <Link class="navbar-brand"to="/"><img className='logostyle' src={logo} alt="" /></Link>
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+        <Link class="nav-link" to ="/home">
+      <i class="fa-solid fa-house"></i>Fleet</Link>
+        </li>
+        <li class="nav-item">
+        <Link  to ="/notifications" className='nav-link'><i class="fa-solid fa-bell"></i>Quality Monitoring</Link>
+        </li>
+        <li class="nav-item">
+        <Link class="nav-link" to ="/aging">
+      <i class="fa-solid fa-clock"></i>Fleet Aging</Link>
+        </li>
+      </ul>
+      <div className='main d-flex'>
+      <span><i class="fa-solid fa-user"></i>John Doe</span>
+      <Link to="/"><i class="fa-solid fa-arrow-right-from-bracket"></i>Sign Out</Link> 
+      </div>
+    </div>
+  </div>
+</nav>
+        </>
+        // <div>
+        //     <nav class="navbar navbar-expand-lg">
+        //         <Link className='navbar-brand' to='/'>
+        //             <img className='logostyle' src={logo} alt="" />
+        //         </Link>
+        //         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        //             <span class="navbar-toggler-icon"></span>
+        //         </button>
+        //         <div class="collapse navbar-collapse" id="navbarNav">
+        //             <ul class="navbar-nav">
+        //                 <li class="nav-item">
+        //                 <Link class="nav-link" to ="/home">
+        //                         <i class="fa-solid fa-house"></i>Fleet</Link>
 
-                        </li>
-                        <li class="nav-item">
-                        <Link to ="/notifications"><a class="nav-link" href="#">
-                                <i class="fa-solid fa-bell"></i>Quality Monitoring</a></Link>
-                        </li>
-                        <li class="nav-item">
-                        <Link to ="/aging"><a class="nav-link" href="#">
-                                <i class="fa-solid fa-clock"></i>Fleet Aging</a></Link>
-                        </li>
-                    </ul>
-                </div>
-                <div className='main'>
-                    <a href="#">
-                    <i class="fa-solid fa-user"></i>John Doe</a>
-                    <a href="#">
-                        <i class="fa-solid fa-arrow-right-from-bracket"></i>Sign Out</a>
-                </div>
-            </nav>
-        </div>
+        //                 </li>
+        //                 <li class="nav-item">
+        //                 <Link class="nav-link" to ="/notifications">
+        //                         <i class="fa-solid fa-bell"></i>Quality Monitoring</Link>
+        //                 </li>
+        //                 <li class="nav-item">
+        //                 <Link class="nav-link" to ="/aging">
+        //                         <i class="fa-solid fa-clock"></i>Fleet Aging</Link>
+        //                 </li>
+        //             </ul>
+        //         </div>
+        //         <div className='main'>
+                   
+        //             <span><i class="fa-solid fa-user"></i>John Doe</span>
+                  
+        //              <Link to="/"><i class="fa-solid fa-arrow-right-from-bracket"></i>Sign Out</Link>  
+        //         </div>
+        //     </nav>
+        // </div>
     )
 }
 
